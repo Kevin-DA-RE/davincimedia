dea20a806356cc978db7bf123decd9681f5c0554
DA RE Kévin <k.dare@fimainfo.fr>
2024-12-12 13:20:51 +0100

    config_onMounted

--- resources/js/views/Movies.vue
+++ resources/js/views/Movies.vue
@@ -2,36 +2,47 @@
 import axios from "axios";
 import { onMounted, ref } from "vue";
 
-const movies = ref([])
-const genres = ref([])
+const moviesList = ref([])
+const genresList =ref([])
+const btnMovie = ref(false)
 
 onMounted( async() => {
-  movies.value = await axios
-  .get(`http://127.0.0.1:8000/api/movie/show-movie`, {
-        headers: {
-        accept: "application/json",
-      },
-    })
-    .then((movie) => movie.data)
-    .catch((error) =>
-      console.log(`Erreur lors de la récupération de datas sur les film \n ${error}`)
-    );
+      try {
+              const movies = await axios
+                  .get(`http://127.0.0.1:8000/api/movie/show-movie`, {
+                        headers: {
+                        accept: "application/json",
+                        },
+                  })
+                  .then((movie) => movie.data)
+                 
+                  if (movies.length > 0) {
+                        console.log("on ajoute des films");
+                        moviesList.value.push(movies)
+                  }
+                  console.log(movies);
+                  
+      } catch (error) {
+            console.log(error);
+            
+      }
 
-  genres.value = await axios
-  .get(`http://127.0.0.1:8000/api/movie/show-genre`, {
-        headers: {
-        accept: "application/json",
-      },
-    })
-    .then((genre) => genre.data)
-    .catch((error) =>
-      console.log(`Erreur lors de la récupération de datas sur les film \n ${error}`)
-    );
+      try {
+             const genres = await axios
+                  .get(`http://127.0.0.1:8000/api/movie/show-genre`, {
+                        headers: {
+                        accept: "application/json",
+                        },
+                  })
+                  .then((genre) => genre.data)
+                  console.log(genres.length);
+      } catch (error) {
+            console.log(error);
+      }
 
-console.log(movies.value);
-console.log(genres.value);
-
-
+    btnMovie.value = (moviesList.value.length < 0) ? false: true;
+      console.log(btnMovie.value);
+      
 })
 
 const props = defineProps({
@@ -41,20 +52,19 @@ const props = defineProps({
 });
 
 </script>
+
 <template>
-  <div v-if="movies.length > 0">
+  <div v-if="moviesList.length > 0">
     <q-drawer class="bg-grey-7 text-white" show-if-above v-model="props.leftDrawerOpen" side="left" bordered>
-    <div v-for="genre in genres" :key="genre.id">
-      <h2>{{ genre.name }}</h2>
-    </div>
+      <div v-for="genre in genresList.value" :key="genre.id">
+            <h2>{{ genre.name }}</h2>
+      </div>
     </q-drawer>
   </div>
   <div v-else>
-    <q-btn color="primary" label="Ajouter film" />
+    <q-btn color="primary" label="Ajouter film" v-show="btnMovie" />
   </div>
-  
-
-
 </template>
+
 <style>
 </style>
