fdb2899a2fc40709711d402805f4eb1b3e63f07f
Kevin <dare.kevin21000@gmail.com>
2025-02-26 23:54:18 +0100

    delete_Auth::check()_in_request

--- app/Http/Requests/MovieRequest.php
+++ app/Http/Requests/MovieRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
 class MovieRequest extends FormRequest
 {
@@ -12,7 +11,7 @@ class MovieRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return Auth::check();
+        return true;
     }
 
     /**
