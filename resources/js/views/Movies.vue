7a2afbaf4a79e90c037500f257105e0462b0feab
DA RE Kévin <k.dare@fimainfo.fr>
2025-03-31 14:06:07 +0200

    responsive_Movies_part_1

--- resources/js/views/Movies.vue
+++ resources/js/views/Movies.vue
@@ -2,7 +2,9 @@
 import axios from "axios";
 import { onMounted, ref,computed } from "vue";
 import Movie from "./component/Movie.vue";
+import { useQuasar } from 'quasar';
 
+const quasar = useQuasar();
 const moviesList = ref([])
 const moviesListLoaded = ref([])
 const moviesListFiltred = ref([])
@@ -301,7 +303,31 @@ const filteredMovies = computed(() => {
 
 </script>
 
+
 <template>
+  <div v-if="quasar.screen.lt.sm" class="bg-dark">
+    <q-btn color="secondary" icon="manage_search" />
+    <q-menu max-height="130px" >
+      <div v-for="genre in genresListLoaded" key="genre.id">
+        <q-list dense>
+          <q-item clickable>
+            <q-item-section>
+              <q-item-label @click="showMoviesWithGenres(genre)">{{ genre.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </q-menu>
+
+    <div class=" bg-dark row justify-start">
+          <div
+          v-for="(movie, index) in filteredMovies" :key="movie.id"
+          >
+              <Movie :movie="movie" />
+          </div>
+    </div>
+  </div>
+  <div v-else>
     <q-splitter
       v-model="splitterModel"
       style="height: 100vh"
@@ -452,6 +478,7 @@ const filteredMovies = computed(() => {
       </template>
 
     </q-splitter>
+  </div>
 </template>
 
 <style>
