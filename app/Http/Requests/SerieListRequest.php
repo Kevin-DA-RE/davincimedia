d075817e03869b129f8b4bffbd18e04beb836069
kevin <dare.kevin21000@gmail.com>
2025-05-03 00:26:21 +0200

    create_SerieListRequest

---
+++ app/Http/Requests/SerieListRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SerieListRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'seriesList.*.id_movie' => ['required', 'integer'],
+            'seriesList.*.name' => ['required', 'string'],
+            'seriesList.*.synopsis' => ['required', 'string'],
+            'seriesList.*.url_img' => ['required', 'string'],
+            'seriesList.*.genres.*.id_genre' => ['required', 'integer'],
+            'seriesList.*.genres.*.name' => ['required', 'string'],
+        ];
+    }
+}
