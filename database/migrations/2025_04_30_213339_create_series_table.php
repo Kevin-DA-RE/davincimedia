33fecf3c78ee8b512b9ed2fc14ecd545c7be6630
kevin <dare.kevin21000@gmail.com>
2025-05-02 23:59:38 +0200

    Revert "update_Series__migration"
    This reverts commit ce95aff8eb9000d17c7d7b2699359098f2afa69e.

--- database/migrations/2025_04_30_213339_create_series_table.php
+++ database/migrations/2025_04_30_213339_create_series_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('series', function (Blueprint $table) {
+        Schema::create('movies', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('id_movie');
             $table->string('name');
