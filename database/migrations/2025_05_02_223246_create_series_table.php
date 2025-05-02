57f0b8216774a4f79a2805d3cc26afce99ee7621
kevin <dare.kevin21000@gmail.com>
2025-05-03 00:33:40 +0200

    create_Serie_Migration

---
+++ database/migrations/2025_05_02_223246_create_series_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_serie');
+            $table->string('name');
+            $table->string('synopsis');
+            $table->string('url_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('series');
+    }
+};
