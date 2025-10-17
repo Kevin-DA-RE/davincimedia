391b6501d7c9156f6937fcb7d766f23ff9dbde63
DA RE Kévin <k.dare@fimainfo.fr>
2025-10-17 15:00:07 +0200

    storage_link _logo_davincimedia

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: "vue/dist/vue.esm-bundler.js",
-            '@img': '/src/img',
+            '@img': '/storage/app/public/img',
         },
     },
     build: {
